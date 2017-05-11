5bc7604455642b81c967ca3977b0953fa3348c26
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-05-11 14:41:10 +0200

    Adds more stats

---
+++ components/Schools.js
@@ -0,0 +1,44 @@
+'use strict'
+
+import React from 'react'
+import Panel from 'muicss/lib/react/panel'
+const getData = require('../lib/get-data')
+
+export default class Status extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      data: []
+    }
+    this.tick = this.tick.bind(this)
+  }
+
+  async componentDidMount () {
+    const data = await getData(this.props.source)
+    this.setState({data: data})
+    this.timer = setInterval(this.tick, parseInt(this.props.refresh || '1', 10) * 1000 * 60)
+  }
+
+  async tick () {
+    const data = await getData(this.props.source)
+    this.setState({data: data})
+  }
+
+  render () {
+    return (
+      <Panel>
+        <h2>{this.props.title}</h2>
+        <table width='100%'>
+          {this.state.data.map((line) => {
+            return (
+              <tr>
+                <td className='mui--text-display1 mui--divider-bottom'>{line._id}</td>
+                <td className='mui--text-display1 mui--text-right mui--divider-bottom'>{line.total}</td>
+              </tr>
+            )
+          })}
+        </table>
+      </Panel>
+    )
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,8 @@ import Col from 'muicss/lib/react/col'
 import Head from '../components/head'
 import Queue from '../components/Queue'
 import Saksbehandling from '../components/Saksbehandling'
+import Schools from '../components/Schools'
+import Total from '../components/Total'
 
 export default class Index extends React.Component {
   render () {
@@ -16,11 +18,19 @@ export default class Index extends React.Component {
         <Container fluid>
           <h1 className='mui--text-title'>MinElev Dashboard</h1>
           <Row>
-            <Col md='6'>
+            <Col md='4'>
               <Queue title='Antall i kø' source='https://tfk-livestats.firebaseio.com/minelev.json' refresh='10' />
             </Col>
-            <Col md='6'>
+            <Col md='4'>
               <Saksbehandling title='Antall behandlet' source='https://seneca-firebase-test.firebaseio.com/tfk-saksbehandling-elev-varsel.json' refresh='10' />
+            </Col>
+            <Col md='4'>
+              <Total title='Antall totalt' source='https://logs.minelev.no/stats/total' refresh='10' />
+            </Col>
+          </Row>
+          <Row>
+            <Col md='12'>
+              <Schools title='Antall pr skole' source='https://logs.minelev.no/stats/schools' refresh='10' />
             </Col>
           </Row>
         </Container>
