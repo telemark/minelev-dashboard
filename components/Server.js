75d1fbe4eb984e8deeca2a89cba2492f1b9c93b8
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-05-14 16:35:57 +0200

    Adds uptime stats

--- components/Server.js
+++ components/Server.js
@@ -2,25 +2,25 @@
 
 import React from 'react'
 import Panel from 'muicss/lib/react/panel'
-const getRss = require('../lib/get-rss')
+const getStatus = require('../lib/get-uptime')
 
 export default class Status extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      data: false
+      data: []
     }
     this.tick = this.tick.bind(this)
   }
 
   async componentDidMount () {
-    const data = await getRss(this.props.source)
+    const data = await getStatus(this.props.source)
     this.setState({data: data})
     this.timer = setInterval(this.tick, parseInt(this.props.refresh || '1', 10) * 1000 * 60)
   }
 
   async tick () {
-    const data = await getRss(this.props.source)
+    const data = await getStatus(this.props.source)
     this.setState({data: data})
   }
 
@@ -29,8 +29,21 @@ export default class Status extends React.Component {
       <Panel>
         <h2>{this.props.title}</h2>
         <ul className='mui-list--unstyled'>
-          {this.state.data ? <li className='mui--text-display4'>{this.state.data || 'Feil'}</li> : null}
+          {this.state.data.map((line) => {
+            return (
+              <li className='mui--text-display1'><span className={line.status === 'UP' ? 'green' : 'red'}>{line.server}</span></li>
+            )
+          })}
         </ul>
+        <style jsx>{`
+          .red {
+            color: red;
+          }
+          .green {
+            color: green;
+          }
+        `}
+        </style>
       </Panel>
     )
   }
--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import Col from 'muicss/lib/react/col'
 import Head from '../components/head'
 import Queue from '../components/Queue'
 import Saksbehandling from '../components/Saksbehandling'
+import Server from '../components/Server'
 import Schools from '../components/Schools'
 import Total from '../components/Total'
 
@@ -29,7 +30,10 @@ export default class Index extends React.Component {
             </Col>
           </Row>
           <Row>
-            <Col md='12'>
+            <Col md='4'>
+              <Server title='Serverstatus' source='https://parser.rss.micro.t-fk.no/?rss=http://rss.uptimerobot.com/u445292-ef20245b7b4433e4e4ab5f34244338d6' refresh='10' />
+            </Col>
+            <Col md='8'>
               <Schools title='Antall pr skole' source='https://logs.minelev.no/stats/schools' refresh='10' />
             </Col>
           </Row>
