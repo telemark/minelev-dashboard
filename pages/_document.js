625e5c9dced678a6953a7758d1909d67dc47048e
Mats Andreassen <matsand@live.no>
2020-07-02 11:19:10 +0200

    Updated icons

--- pages/_document.js
+++ pages/_document.js
@@ -13,12 +13,18 @@ class MyDocument extends Document {
           <link rel='apple-touch-icon' sizes='180x180' href='/static/apple-touch-icon.png' />
           <link rel='icon' type='image/png' sizes='32x32' href='/static/favicon-32x32.png' />
           <link rel='icon' type='image/png' sizes='16x16' href='/static/favicon-16x16.png' />
-          <link rel='manifest' href='/static/manifest.json' />
-          <link rel='mask-icon' href='/static/safari-pinned-tab.svg' color='#5bbad5' />
+          <link rel='mask-icon' href='/static/safari-pinned-tab.svg' color='#005260' />
           <link rel='shortcut icon' href='/static/favicon.ico' />
-          <meta name='msapplication-TileColor' content='#da532c' />
-          <meta name='msapplication-config' content='/static/browserconfig.xml' />
+          <meta name='msapplication-TileColor' content='#005260' />
           <meta name='theme-color' content='#ffffff' />
+
+          <link rel='manifest' href='/static/site.webmanifest' />
+          <meta name='msapplication-config' content='/static/browserconfig.xml' />
+
+          <link href='https://fonts.googleapis.com/icon?family=Material+Icons' rel='stylesheet' />
+          <link href='https://fonts.googleapis.com/css2?family=Nunito+Sans' rel='stylesheet' />
+          <link href='https://fonts.googleapis.com/css2?family=Nunito' rel='stylesheet' />
+
           <meta name='viewport' content='width=device-width, initial-scale=1.0' />
           <meta name='description' content='Dashboard for MinElev plattformen' />
         </Head>
